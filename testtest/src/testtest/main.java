7b2608e5a8a58cdd4fd89fc6db2f75ddc55e4122
ycshen99 < ycshen99@gmail.com >
2014-10-29 14:35:44 +0800

    Complete - task : test

---
+++ testtest/src/testtest/main.java
@@ -0,0 +1,13 @@
+package testtest;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//String word = Messages.getString("main.0"); //$NON-NLS-1$
+		System.out.println("abc");
+	}
+
+}
