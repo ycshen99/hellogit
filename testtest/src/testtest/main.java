64dda44aa631c454c2e0cfb779c279f3fc773555
gem updta rails <checkincheckin@yahoo.com.tw>
2014-10-29 15:03:42 +0800

    Complete - task : test

--- testtest/src/testtest/main.java
+++ testtest/src/testtest/main.java
@@ -7,7 +7,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//String word = Messages.getString("main.0"); //$NON-NLS-1$
-		System.out.println("abcde");
+		System.out.println("abcdefg");
 	}
 
 }
